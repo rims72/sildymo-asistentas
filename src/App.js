6495e8de33315b3f076f8c2ed5bb577b42889278
rimantas Marcinkus <rimantasmarcinkus@MacBook-Pro.local>
2025-06-06 11:27:13 +0300

    Pradinis pridėjimas iš ZIP

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeatingAssistant from "./HeatingAssistant";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 py-8">
+      <HeatingAssistant />
+    </div>
+  );
+}
+
+export default App;
